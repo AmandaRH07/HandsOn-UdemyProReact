7d4788ebd5f5dbf8e3b9980799e3e38e67889f43
Amanda Rafaela Hass <amanda.hass@ambevtech.com.br>
2022-07-16 10:58:23 -0300

    Finalizado

--- Search_Filter/search-filter/src/App.js
+++ Search_Filter/search-filter/src/App.js
@@ -1,21 +1,43 @@
+import { useState, useEffect } from 'react';
 import TicketsFilter from './components/TicketsFilter';
-
+import TicketsList from './components/TicketsList';
+import { dummyTickets } from './data/tickets-data';
+ 
 function App() {
+  const [supportTickets, setSupportTickets] = useState(dummyTickets);
+  const [statusFilter, setStatusFilter] = useState('all');
+  const [assigneeFilter, setAssigneeFilter] = useState('all');
+  const [ticketSeachTerm, setTicketSeachTerm] = useState('');
+ 
+  useEffect(function(){
+    setSupportTickets(dummyTickets.filter(function(ticket){
+      const ticketHasFilteredStatus = statusFilter === 'all' || statusFilter === ticket.status;
+      const ticketHasFilteredAssignee = assigneeFilter === 'all' || assigneeFilter === ticket.assignee;
+      const normalizedTitle = ticket.title.toLocaleLowerCase();
+      const normalizedText = ticket.text.toLocaleLowerCase();
+      const normalizedSearchTerm = ticketSeachTerm.toLowerCase();
+
+      const ticketHasSearchTerm = normalizedTitle.includes(normalizedSearchTerm) || normalizedText.includes(normalizedSearchTerm);
+    
+      return ticketHasFilteredStatus && ticketHasFilteredAssignee && ticketHasSearchTerm
+    }))
+  }, [statusFilter, assigneeFilter, ticketSeachTerm]);
+
   function findTicketsHandler(searchTerm) {
-    // Todo
+    setTicketSeachTerm(searchTerm);
   }
 
   function changeStatusHandler(newStatus) {
-    // Todo
+    setStatusFilter(newStatus);
   }
 
   function changeAssigneeHandler(newAssignee) {
-    // Todo
+    setAssigneeFilter(newAssignee);
   }
 
   return (
     <main>
-      {/* Missing: List of tickets */}
+      <TicketsList tickets={supportTickets}/>
       <TicketsFilter
         onChangeStatus={changeStatusHandler}
         onChangeAssignee={changeAssigneeHandler}
